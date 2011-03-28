1fbe0d80a2ed7ea01b9e739a408f81258479c7f2
ggarcia <giba.dmb@gmail.com>
2011-03-28 18:37:42 -0300

    First kata - find the sum all multiples of 3 or 5 less than 1000 Fourth solution. Method sum was refactored and it now receives an array 'multiples' as param  that is used to filter the numbers that will be summed up. This is a more generic solution since it accepts the upper limit and and an array of multiples as parameters. And, now we can sum up the multiples of any upper limit number using any combination of multiples.

--- ruby/katas/01/first_kata.rb
+++ ruby/katas/01/first_kata.rb
@@ -1,11 +1,23 @@
 class FirstKata
-  def sum(upper_limit, multiple)
+  def sum(upper_limit, multiples)
     sum = 0
 
     upper_limit.to_i.times do |i|
-      sum += i if (i % multiple == 0)
+      sum += i if is_multiple?(i, multiples)
     end
 
     sum
+  end
+
+  def is_multiple?(number, multiples)
+    multiple = false
+
+    multiples.each do |j|
+      if (number % j == 0)
+        multiple = true
+      end
+    end
+
+    multiple
   end
 end
--- ruby/test/01/first_kata_test.rb
+++ ruby/test/01/first_kata_test.rb
@@ -3,6 +3,7 @@ require "../../katas/01/first_kata"
 
 class FirstKataTest < Test::Unit::TestCase
   def test_sum
-    assert_equal(18, FirstKata.new.sum(10, 3), "result should be 18")
+    assert_equal(23, FirstKata.new.sum(10, [3,5]), "result should be 23")
+    assert_equal(233168, FirstKata.new.sum(1000, [3,5]), "result should be 233168")
   end
 end
