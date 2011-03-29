94df2c7c26c6580f265d3612df30227ba901b7ce
ggarcia <giba.dmb@gmail.com>
2011-03-29 16:25:26 -0300

    First kata - find the sum all multiples of 3 or 5 less than 1000 Fourth solution. Methods sum and isMultiple was refactored in order to get a more functional code. Notice that the code is a bit more concise now.

--- scala/main/scala/src/katas/one/FirstKata.scala
+++ scala/main/scala/src/katas/one/FirstKata.scala
@@ -4,20 +4,13 @@ class FirstKata {
   def sum(upperLimit: Int, multiples: Seq[Int]): Int = {
     var sum: Int = 0;
 
-    for (i <- 1 until upperLimit) {
-      if (isMultiple(i, multiples))
-        sum = sum + i
-    }
+    for (i <- 1 until upperLimit if isMultiple(i, multiples))
+    yield sum = sum + i
 
     sum
   }
 
   def isMultiple(upperLimit: Int, multiples: Seq[Int]): Boolean = {
-    for (multiple <- multiples) {
-      if (upperLimit % multiple == 0)
-        return true
-    }
-
-    return false
+    if (multiples.filter(upperLimit % _ == 0).isEmpty) false else true
   }
 }
