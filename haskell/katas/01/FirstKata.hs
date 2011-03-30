7ebbc36e9534a8654534c724d16deb31ac885c66
ggarcia <ggarcia@eureka.inf.br>
2011-03-29 22:54:01 -0300

    First kata - find the sum all multiples of 3 or 5 less than 1000 Third solution. Refactoring fkSum to receive a list of multiples. Now we have a very generic method to calculate the sum of multiples of a given list less than an upperLimit

--- haskell/katas/01/FirstKata.hs
+++ haskell/katas/01/FirstKata.hs
@@ -2,5 +2,10 @@ module FirstKata (
   fkSum
 ) where
 
-fkSum :: Int -> Int
-fkSum a = foldl (+) 0 (filter (\x -> (mod x 3 == 0) || (mod x 5 == 0)) [1..a])
+fkSum :: Int -> [Int] -> Int
+fkSum a [] = 0
+fkSum a (b) = foldl (+) 0 (filter (\x -> isMultiple x b) [1..a])
+
+isMultiple :: Int -> [Int] -> Bool
+isMultiple a [] = False
+isMultiple a (x:xs) = if (mod a x == 0) then True else isMultiple a xs
--- haskell/test/01/FirstKataTest.hs
+++ haskell/test/01/FirstKataTest.hs
@@ -1,5 +1,7 @@
 import Test.HUnit
 import FirstKata 
 
-testFkSum = TestCase $ assertEqual "result should be 233168" 233168 (fkSum 999)
+testFkSumFirst = TestCase $ assertEqual "result should be 23" 23 (fkSum 9 [3,5])
+testFkSumSecond = TestCase $ assertEqual "result should be 233168" 233168 (fkSum 999 [3,5])
 
+suite = TestList [ testFkSumFirst, testFkSumSecond ]
