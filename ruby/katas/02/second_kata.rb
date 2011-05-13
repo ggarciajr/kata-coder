8787924975095185431f88844a28957dfef4de68
ggarcia <giba.dmb@gmail.com>
2011-05-13 16:22:16 -0300

    SecondKata
    Tentative #2

--- ruby/katas/02/second_kata.rb
+++ ruby/katas/02/second_kata.rb
@@ -1,16 +1,17 @@
 class SecondKata
   def sum(limit)
     sum = 0
+	n = ((Math.log(limit.to_i * (Math.sqrt 5)) * 2) + 1.618).round
 	
-    limit.to_i.times do |i|
-	  fibN = fib(i)
+    (6..n).each do |i|
+	  fibN = (4 * fib(i - 3)) + fib(i - 6)
 	  
 	  sum += fibN if fibN % 2 == 0
 	  
 	  break if fibN > limit
 	end
 	
-	sum
+	sum + 2
   end
   
   def fib(n)
